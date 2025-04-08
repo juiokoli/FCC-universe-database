097499531783b5540a72ea84374dbdb1454b212d
juiokoli <dominikstanek06@gmail.com>
2025-04-08 21:58:15 +0200

    Add files via upload

--- universe.sql
+++ universe.sql
@@ -16,6 +16,29 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE universe;
+--
+-- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE universe OWNER TO freecodecamp;
+
+\connect universe
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -246,87 +269,77 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.galaxy (galaxy_id, name, type, discovered_year, planets_inside) FROM stdin;
-1	Milky way	Barred spiral	1700	100B
-2	Andromeda	Barred spiral	-961	trillions
-3	Messier 87	Elliptical	1781	billions
-4	Whirlpool	Spiral	1773	billions
-5	Sombrero	Spiral	1781	800B
-6	Triangulum	Spiral	1654	billions
-\.
+INSERT INTO public.galaxy VALUES (1, 'Milky way', 'Barred spiral', 1700, '100B');
+INSERT INTO public.galaxy VALUES (2, 'Andromeda', 'Barred spiral', -961, 'trillions');
+INSERT INTO public.galaxy VALUES (3, 'Messier 87', 'Elliptical', 1781, 'billions');
+INSERT INTO public.galaxy VALUES (4, 'Whirlpool', 'Spiral', 1773, 'billions');
+INSERT INTO public.galaxy VALUES (5, 'Sombrero', 'Spiral', 1781, '800B');
+INSERT INTO public.galaxy VALUES (6, 'Triangulum', 'Spiral', 1654, 'billions');
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.moon (moon_id, name, planet_id, size, gravity) FROM stdin;
-1	Luna	1	3474.2	1.62
-2	Phobos	2	22.2	0.0057
-3	Deimos	2	12.6	0.003
-4	Io	3	3643.2	1.796
-5	Europa	3	3121.6	1.315
-6	Ganymede	3	5268.2	1.428
-7	Callisto	3	4820.6	1.235
-8	Titan	4	5149.5	1.352
-9	Enceladus	4	504.2	0.113
-10	Mimas	4	396.4	0.064
-11	Rhea	4	1527.6	0.264
-12	Iapetus	4	1468.6	0.223
-13	Tethys	4	1062.2	0.146
-14	Dione	4	1122.8	0.232
-15	Hyperion	4	270.0	0.02
-16	Betel-1	6	2345.6	1.24
-17	Betel-2	6	1543.2	0.87
-18	Arcturus Moon Prime	8	4567.8	2.1
-19	Andromeda Minor	9	567.3	0.45
-20	Sirius Moon Alpha	12	890.5	0.67
-\.
+INSERT INTO public.moon VALUES (1, 'Luna', 1, 3474.2, 1.62);
+INSERT INTO public.moon VALUES (2, 'Phobos', 2, 22.2, 0.0057);
+INSERT INTO public.moon VALUES (3, 'Deimos', 2, 12.6, 0.003);
+INSERT INTO public.moon VALUES (4, 'Io', 3, 3643.2, 1.796);
+INSERT INTO public.moon VALUES (5, 'Europa', 3, 3121.6, 1.315);
+INSERT INTO public.moon VALUES (6, 'Ganymede', 3, 5268.2, 1.428);
+INSERT INTO public.moon VALUES (7, 'Callisto', 3, 4820.6, 1.235);
+INSERT INTO public.moon VALUES (8, 'Titan', 4, 5149.5, 1.352);
+INSERT INTO public.moon VALUES (9, 'Enceladus', 4, 504.2, 0.113);
+INSERT INTO public.moon VALUES (10, 'Mimas', 4, 396.4, 0.064);
+INSERT INTO public.moon VALUES (11, 'Rhea', 4, 1527.6, 0.264);
+INSERT INTO public.moon VALUES (12, 'Iapetus', 4, 1468.6, 0.223);
+INSERT INTO public.moon VALUES (13, 'Tethys', 4, 1062.2, 0.146);
+INSERT INTO public.moon VALUES (14, 'Dione', 4, 1122.8, 0.232);
+INSERT INTO public.moon VALUES (15, 'Hyperion', 4, 270.0, 0.02);
+INSERT INTO public.moon VALUES (16, 'Betel-1', 6, 2345.6, 1.24);
+INSERT INTO public.moon VALUES (17, 'Betel-2', 6, 1543.2, 0.87);
+INSERT INTO public.moon VALUES (18, 'Arcturus Moon Prime', 8, 4567.8, 2.1);
+INSERT INTO public.moon VALUES (19, 'Andromeda Minor', 9, 567.3, 0.45);
+INSERT INTO public.moon VALUES (20, 'Sirius Moon Alpha', 12, 890.5, 0.67);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.planet (planet_id, name, number_of_moons, mass, description, has_rings, star_id) FROM stdin;
-1	Earth	1	5.97	Blue planet with abundant water and life	f	1
-2	Mars	2	0.642	Red planet with thin atmosphere	f	1
-3	Jupiter	79	1898	Largest planet in our solar system with a giant red spot	t	1
-4	Saturn	82	568	Beautiful planet with prominent ring system	t	1
-5	Proxima Centauri b	0	1.27	Potentially habitable exoplanet orbiting Proxima Centauri	f	1
-6	Betelgeuse I	5	2543	Giant hot planet orbiting the red supergiant	f	2
-7	Betelgeuse II	3	1125	Rocky world with extreme temperatures	f	2
-8	Arcturus Prime	12	857	Gas giant with complex weather systems	t	3
-9	Andromeda Major	8	1235	Distant exoplanet with methane atmosphere	t	4
-10	Alcyone III	2	2.5	Small rocky world with unusual magnetic field	f	5
-11	Sirius A-1	0	0.5	Hot terrestrial planet close to its star	f	6
-12	Sirius A-2	4	15.3	Cool gas giant far from its star	t	6
-\.
+INSERT INTO public.planet VALUES (1, 'Earth', 1, 5.97, 'Blue planet with abundant water and life', false, 1);
+INSERT INTO public.planet VALUES (2, 'Mars', 2, 0.642, 'Red planet with thin atmosphere', false, 1);
+INSERT INTO public.planet VALUES (3, 'Jupiter', 79, 1898, 'Largest planet in our solar system with a giant red spot', true, 1);
+INSERT INTO public.planet VALUES (4, 'Saturn', 82, 568, 'Beautiful planet with prominent ring system', true, 1);
+INSERT INTO public.planet VALUES (5, 'Proxima Centauri b', 0, 1.27, 'Potentially habitable exoplanet orbiting Proxima Centauri', false, 1);
+INSERT INTO public.planet VALUES (6, 'Betelgeuse I', 5, 2543, 'Giant hot planet orbiting the red supergiant', false, 2);
+INSERT INTO public.planet VALUES (7, 'Betelgeuse II', 3, 1125, 'Rocky world with extreme temperatures', false, 2);
+INSERT INTO public.planet VALUES (8, 'Arcturus Prime', 12, 857, 'Gas giant with complex weather systems', true, 3);
+INSERT INTO public.planet VALUES (9, 'Andromeda Major', 8, 1235, 'Distant exoplanet with methane atmosphere', true, 4);
+INSERT INTO public.planet VALUES (10, 'Alcyone III', 2, 2.5, 'Small rocky world with unusual magnetic field', false, 5);
+INSERT INTO public.planet VALUES (11, 'Sirius A-1', 0, 0.5, 'Hot terrestrial planet close to its star', false, 6);
+INSERT INTO public.planet VALUES (12, 'Sirius A-2', 4, 15.3, 'Cool gas giant far from its star', true, 6);
 
 
 --
 -- Data for Name: space_station; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.space_station (name, location, crew_size, distance_from_earth, space_station_id) FROM stdin;
-International Space Station	Earth Orbit	7	408	1
-New Horizons Station	Jupiter Orbit	15	778500000	2
-Andromeda Research Base	Andromeda Galaxy	35	2537000000000000	3
-\.
+INSERT INTO public.space_station VALUES ('International Space Station', 'Earth Orbit', 7, 408, 1);
+INSERT INTO public.space_station VALUES ('New Horizons Station', 'Jupiter Orbit', 15, 778500000, 2);
+INSERT INTO public.space_station VALUES ('Andromeda Research Base', 'Andromeda Galaxy', 35, 2537000000000000, 3);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.star (star_id, name, is_active, galaxy_id, size) FROM stdin;
-1	Alpha Centauri A	t	1	1.22
-2	Betelgeuse	t	1	1000
-3	Arcturus	t	1	25.4
-4	Andromeda I	t	2	1.2
-5	Alcyone	t	1	2.9
-6	Sirius	t	1	1.71
-\.
+INSERT INTO public.star VALUES (1, 'Alpha Centauri A', true, 1, 1.22);
+INSERT INTO public.star VALUES (2, 'Betelgeuse', true, 1, 1000);
+INSERT INTO public.star VALUES (3, 'Arcturus', true, 1, 25.4);
+INSERT INTO public.star VALUES (4, 'Andromeda I', true, 2, 1.2);
+INSERT INTO public.star VALUES (5, 'Alcyone', true, 1, 2.9);
+INSERT INTO public.star VALUES (6, 'Sirius', true, 1, 1.71);
 
 
 --
@@ -445,27 +458,27 @@ ALTER TABLE ONLY public.space_station
 
 
 --
--- Name: star galaxy_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: star galaxy_id_ref; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.star
-    ADD CONSTRAINT galaxy_id FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+    ADD CONSTRAINT galaxy_id_ref FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
 
 
 --
--- Name: moon planet_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: moon planet_id_ref; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.moon
-    ADD CONSTRAINT planet_id FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+    ADD CONSTRAINT planet_id_ref FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
 
 
 --
--- Name: planet star_id; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: planet star_id_ref; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.planet
-    ADD CONSTRAINT star_id FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+    ADD CONSTRAINT star_id_ref FOREIGN KEY (star_id) REFERENCES public.star(star_id);
 
 
 --
